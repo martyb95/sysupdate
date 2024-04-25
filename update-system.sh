45715e34b707bcf3b7ddf831f277220897f75d12
martyb95 <77902243+martyb95@users.noreply.github.com>
2024-04-25 13:09:17 -0400

    Add files via upload

--- update-system.sh
+++ update-system.sh
@@ -1922,7 +1922,7 @@ function _title() {
         ███████║███████╗   ██║   ╚██████╔╝██║
         ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝
 "
-   printf "\n\t\t   ${YELLOW}${OS^^} System Setup        ${LPURPLE}Ver 2.66\n${RESTORE}"
+   printf "\n\t\t   ${YELLOW}${OS^^} System Setup        ${LPURPLE}Ver 2.67\n${RESTORE}"
    printf "\t\t\t\t\t${YELLOW}by: ${LPURPLE}Martin Boni${RESTORE}\n"
 }
 
