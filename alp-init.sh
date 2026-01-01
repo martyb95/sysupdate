21c0e8acfa08c2fc7c85d944fa90659ae672a786
martyb95 <77902243+martyb95@users.noreply.github.com>
2026-01-01 16:41:04 -0500

    Add files via upload

--- alp-init.sh
+++ alp-init.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/ash
 
 #=========================================================
 #      Color Codes
@@ -94,12 +94,12 @@ function setupRepo {
       LST=$(wget -qO- https://mirrors.alpinelinux.org/mirrors.txt)
 
       #Remove mirrors that are known not to respond
-      LST=$(grep -v "mirror.lzu.edu.cn" <<< "$LST")
-      LST=$(grep -v "mirror.leitecastro.com" <<< "$LST")
-      LST=$(grep -v "mirror.serverion.com" <<< "$LST")
-      LST=$(grep -v "repo.jing.rocks" <<< "$LST")
-      LST=$(grep -v "mirror.siwoo.org" <<< "$LST")
-      LST=$(grep -v "mirror.saddle.netowrk" <<< "$LST")
+      LST=$(cat "$LST" | grep -v "mirror.lzu.edu.cn")
+      LST=$(cat "$LST" | grep -v "mirror.leitecastro.com")
+      LST=$(cat "$LST" | grep -v "mirror.serverion.com")
+      LST=$(cat "$LST" | grep -v "repo.jing.rocks")
+      LST=$(cat "$LST" | grep -v "mirror.siwoo.org")
+      LST=$(cat "$LST" | grep -v "mirror.saddle.netowrk")
 
       #Test the mirrors in the list
       for src in "$LST"; do
